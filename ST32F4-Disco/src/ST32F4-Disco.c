c3ebaff5fbb34f7c95abe132190938fe51fc5ba2
michel <sanchesm8@gmail.com>
2014-03-01 21:30:50 +0100

    creating not yet building

---
+++ ST32F4-Disco/src/ST32F4-Disco.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ST32F4-Disco.c
+ Author      : michel sanches
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
